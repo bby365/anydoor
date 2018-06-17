6724d63076316a6b08baa34c26da17c81fbe5cab
bby365 <807167164@qq.com>
2018-06-17 20:56:31 +0800

    finished

--- src/app.js
+++ src/app.js
@@ -2,14 +2,25 @@ const http = require('http');
 const path = require('path');
 const fs   = require('fs');
 const conf = require('./config/defaultConfig.js');
-const route = require('./helper/route.js')
+const route = require('./helper/route.js');
+const openUrl = require('./helper/openUrl');
 
-const server = http.createServer((req,res)=>{
-    const filePath = path.join(conf.root,req.url);
-    route(req,res,filePath);
-  
-});
-server.listen(conf.port,conf.hostname, ()=>{
-    const addr = `http://${conf.hostname}:${conf.port}`;
-    console.log(`Server started at ${addr}`)
-});
+class Server{
+    constructor(config){
+        this.conf = Object.assign({},conf,config);
+    }
+    start(){
+        const server = http.createServer((req,res)=>{
+            const filePath = path.join(this.conf.root,req.url);
+            route(req,res,filePath,this.conf);
+          
+        });
+        server.listen(this.conf.port,this.conf.hostname, ()=>{
+            const addr = `http://${this.conf.hostname}:${this.conf.port}`;
+            console.log(`Server started at ${addr}`);
+            openUrl(addr);
+        });
+    }
+}
+
+module.exports = Server;
